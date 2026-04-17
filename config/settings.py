83b610fc6c43c7f8929ac06ec06198ceaf1446bb
Johan Martinez <zeamartinezjohan@gmail.com>
2026-04-17 13:53:49 -0500

    fix: set static root for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -160,5 +161,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Auth
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
